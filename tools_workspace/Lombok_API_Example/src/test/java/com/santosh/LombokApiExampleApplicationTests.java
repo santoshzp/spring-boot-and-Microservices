1a1623e3d51cc10321b227e1c8fee1ee1fe913f4
Santosh Zirpe <santoshzp55@gmail.com>
2021-07-30 02:15:35 +0530

    project added

---
+++ tools_workspace/Lombok_API_Example/src/test/java/com/santosh/LombokApiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santosh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LombokApiExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
